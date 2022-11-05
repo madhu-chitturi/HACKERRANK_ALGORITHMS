70aa6f7dc44a1ab19e8d5fb463140b31809d4fb8
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2022-11-05 20:10:03 +0530

    Big Sorting.py

---
+++ Big Sorting.py
@@ -0,0 +1,9 @@
+                                                              Big Sorting
+  
+t = int(input())
+unsorted = []
+for _ in range(t):
+    unsorted.append(input())    
+unsorted.sort(key=int)
+for s in unsorted:
+    print(s)
